a5bf6add9bf0da9bcf8fb5b041f4103017d7b450
Tauriq Hendricks <tauriqh786@gmail.com>
2021-02-21 12:29:46 +0200

    first commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,34 @@
-import { Component } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements OnInit{
   title = 'fashionclient';
+  users: AppUser;
+
+  constructor(private http: HttpClient) {}
+
+  ngOnInit(): void {
+    this.getUsers();
+  }
+
+  getUsers():void {
+    this.http.get<AppUser>('https://localhost:5001/api/users').subscribe(
+      (response: AppUser) => {
+        this.users = response;
+      }, error => {
+        console.log(error);
+      }
+    );
+  }
+
+}
+
+export class AppUser{
+  Id: number;
+  UserName: string;
 }
